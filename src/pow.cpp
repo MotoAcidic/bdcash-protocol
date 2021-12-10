712590e995542696e611ffdf4163ec15ca48aa29
Tfinch <nexusnow@protonmail.com>
2021-12-09 21:50:13 -0500

    Correct block height

--- src/pow.cpp
+++ src/pow.cpp
@@ -39,7 +39,7 @@ unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHead
         int64_t nTargetTimespan = 60 * 40;
     
 
-        if (ActiveProtocol() >= TIME_CHANGE && pindexLast->nHeight > 206000){
+        if (ActiveProtocol() >= TIME_CHANGE && pindexLast->nHeight > 206127){
             nTargetSpacing = 120;
             nTargetTimespan = 30;
         }
